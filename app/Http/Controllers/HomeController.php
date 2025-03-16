1473a2aa7424cc2c6bc523a313b1e859f46089b4
Kirula Upadya <kirulapadukka@gmail.com>
2025-03-17 01:03:43 +0530

    Login and Signup are created.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) {
+            $usertype = Auth::user()->usertype;
+
+            if ($usertype == 'admin') {
+                return view('admin.index');
+            } elseif ($usertype == 'user') {
+                return view('dashboard');
+            }
+        }
+    }
+}
