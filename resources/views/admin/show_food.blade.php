305db6ef3c26b9a9e1ad346fe5491acbb1553edb
Kirula Upadya <kirulapadukka@gmail.com>
2025-03-19 19:57:53 +0530

    Implement data retrieval functionality from database

--- resources/views/admin/show_food.blade.php
+++ resources/views/admin/show_food.blade.php
@@ -5,6 +5,26 @@
 
   @include('admin.css')
 
+  <style>
+    table {
+      border: 1px solid skyblue;
+      margin: auto;
+      width: 800px;
+    }
+
+    th {
+      background-color: skyblue;
+      color: white;
+      padding: 10px;
+      margin: 10px;
+    }
+
+    td {
+      color: white;
+      padding: 10px;
+    }
+  </style>
+
 </head>
 
 <body>
@@ -17,6 +37,34 @@
   <div class="page-content">
     <div class="page-header">
       <div class="container-fluid">
+
+        <h1>All Foods</h1>
+
+        <div>
+
+          <table>
+            <tr>
+              <th>Food Title</th>
+              <th>Details</th>
+              <th>Price</th>
+              <th>Image</th>
+            </tr>
+
+            @foreach($data as $data)
+
+            <tr>
+              <td>{{$data->title}}</td>
+              <td>{{$data->detail}}</td>
+              <td>{{$data->price}}</td>
+              <td>
+                <img width="130" src="food_img/{{$data->image}}" alt="">
+              </td>
+            </tr>
+
+            @endforeach
+          </table>
+
+        </div>
 
 
 
